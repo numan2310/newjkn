115681d472e8b3bcac92beb2fc8573a5caee9a21
numan2310 <pathan23n@gmail.com>
2025-08-22 12:11:39 +0530

    Add files via upload

---
+++ samplewebapp_with_pmd/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins with PMD and JaCoCo!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
